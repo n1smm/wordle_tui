7607aa69e8de3a2a8a2e55e557317cc329135771
Alessandro Russo <alerusso@c2r5p12.42firenze.it>
2025-04-09 15:28:30 +0200

    manca parsing

--- srcs/backend/debug_main.cpp
+++ srcs/backend/debug_main.cpp
@@ -6,7 +6,7 @@
 /*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:16:28 by alerusso          #+#    #+#             */
-/*   Updated: 2025/04/09 15:13:35 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/04/09 15:27:08 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	main()
 {
+	char	guess[6];
+
 	BackendInit();
+	printf("Random word:\t%s\n", RandomWord());
+	printf("Try to guess.\n");
+	read(0, guess, 5);
 	BackendQuit();
 	return (0);
 }
